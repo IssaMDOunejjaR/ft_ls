91fe7abaf219a3ba343716689bd9185a65b4931a
IssaMDOunejjaR <otaku6issam@gmail.com>
2023-09-17 21:28:26 +0100

    working on printing the directory files

--- main.c
+++ main.c
@@ -1,40 +1,47 @@
 #include "ft_ls.h"
+#include "lib/lib.h"
+#include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct s_params {
-  char **paths;
-  char *options;
-} t_params;
-
-const char ALLOWED_OPTIONS[] = {'l', 'R', 'a', 'r', 't'};
-
-int is_allowed_option(char option) {
-  for (int i = 0; i < 5; i++) {
-    if (option == ALLOWED_OPTIONS[i])
-      return 1;
-  }
-
-  return 0;
-}
-
-void print_list(t_params *params, char *options) {}
-
-t_params *parse_params(int argc, char **argv) {
-  t_params *params = malloc(sizeof(t_params));
-
-  for (int i = 1; i < argc; i++) {
-    if (argv[i][0] == '-') {
-    }
-  }
-
-  return params;
-}
 
 int main(int argc, char **argv) {
-  t_params *params = parse_params(argc, argv);
+  t_params params;
 
-  free(params);
+  init_params(&params);
+
+  parse_params(&params, argc, argv);
+
+  for (int i = 0; params.paths[i] != NULL; i++) {
+    DIR *dir = opendir(params.paths[i]);
+
+    if (!dir) {
+      int fd = open(params.paths[i], O_RDONLY);
+
+      if (fd == -1) {
+        no_dir_error(params.paths[i]);
+        free_params(&params);
+
+        return -2;
+      }
+
+      ft_putendl_fd(1, params.paths[i]);
+
+      close(fd);
+
+      continue;
+    }
+
+    print_dir_content(dir, &params, params.paths[i]);
+
+    closedir(dir);
+  }
+
+  ft_putendl_fd(1, "==========================");
+
+  for (int i = 0; params.paths[i] != NULL; i++) {
+    printf("%s\n", params.paths[i]);
+  }
+
+  printf("options = [%s]\n", params.options);
 
   return 0;
 }
