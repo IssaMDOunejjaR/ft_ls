91b4156da0896b3390ff37bf7eac542df93698d3
IssaMDounejjaR <otaku6issam@gmail.com>
2024-09-15 13:11:29 +0100

    cleanup

--- utils/errors.c
+++
@@ -1,7 +0,0 @@
-#include "../ft_ls.h"
-
-void no_dir_error(char *name) {
-  ft_putstr_fd(2, "ft_ls: cannot access '");
-  ft_putstr_fd(2, name);
-  ft_putendl_fd(2, "': No such file or directory");
-}
