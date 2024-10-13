4d70e2b550707846dd55b679badf1cb57ebe3cf9
IssaMDounejjaR <otaku6issam@gmail.com>
2024-10-13 15:32:56 +0100

    testing

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ RED="\033[1;31m"
 ls="/bin/ls"
 ft_ls="./ft_ls"
 
-test_dir="."
+test_dir=".."
 
 options=$($ft_ls --help | grep -oE '^ *-[a-zA-Z0-9]' | sort | uniq | tr -d '\n -')
 
@@ -16,7 +16,7 @@ ls_result="ls.txt"
 ft_ls_result="ft_ls.txt"
 
 is_exception() {
-  exceptions="gl"
+  exceptions="glo"
   
   for char in $(echo $exceptions | fold -w1); do
     if [ "$char" = "$1" ]; then
@@ -31,8 +31,6 @@ for opt in $(echo $options | fold -w1); do
   echo -n "[ $opt ] : "
   
   if is_exception $opt ; then
-    unbuffer $ls -$opt $test_dir | expand -t 8 | grep -v $ls_result | grep -v $ft_ls_result
-    echo "========="
     unbuffer $ls -$opt $test_dir | expand -t 8 | grep -v $ls_result | grep -v $ft_ls_result > $ls_result 2>&1
     $ft_ls -$opt $test_dir | sed 's/\x1B\[[0-9;]*[a-zA-Z]//g' | grep -v $ls_result | grep -v $ft_ls_result > $ft_ls_result 2>&1
   else
